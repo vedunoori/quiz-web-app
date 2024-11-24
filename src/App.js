5a90efe9dbe9117369a97c27c71901992c54c16b
sridhar <vedunoori.sreedhar@gmail.com>
2024-11-24 21:15:40 +0530

    quiz app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import QuizState from './context/QuizState';
+import Home from './pages/Home/Home';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+import About from './pages/About/About';
+import ReviewAnswer from './pages/Review/ReviewAnswer';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <QuizState>
+        <div className="App">
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/review" element={<ReviewAnswer />} />
+          </Routes>
+        </div>
+      </QuizState>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { extendTheme, ChakraProvider, CSSReset, ThemeProvider } from '@chakra-ui/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+};
+
+const fonts = {
+  body: 'Roboto, sans-serif', // Replace 'Roboto' with your desired font
+  heading: 'Roboto, sans-serif', // Replace 'Roboto' with your desired font
+};
+
+const theme = extendTheme({ colors, fonts });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
